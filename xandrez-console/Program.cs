5a9b0d6dfcefc164a072d82506a23b8b239b0416
Henrique de Padua <henriquedepadua@yahoo.com.br>
2019-06-02 14:52:54 -0300

    Criando Classe Posicao

--- xandrez-console/Program.cs
+++ xandrez-console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using tabuleiro;
 
 namespace xandrez_console
 {
@@ -6,7 +7,11 @@ namespace xandrez_console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
         }
     }
 }
