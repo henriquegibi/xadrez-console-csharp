da9929dd98e1384869500a841a39f7413885df04
Henrique de Padua <henriquedepadua@yahoo.com.br>
2019-06-26 10:17:49 -0300

    Jogada especial En Passant

--- xandrez-console/xadrez/Peao.cs
+++ xandrez-console/xadrez/Peao.cs
@@ -4,7 +4,11 @@ namespace xadrez
 {
     class Peao : Peca
     {
-        public Peao(Tabuleiro tab, Cor cor) : base(tab, cor) { }
+        private PartidaDeXadrez partida;
+
+        public Peao(Tabuleiro tab, Cor cor, PartidaDeXadrez partida) : base(tab, cor) {
+            this.partida = partida;
+        }
 
         public override string ToString() { return "P"; }
 
@@ -22,31 +26,6 @@ namespace xadrez
         public override bool[,] movimentosPossiveis()
         {
             bool[,] mat = new bool[tab.linhas, tab.colunas];
-
-            Posicao pos = new Posicao(0, 0);
-
-            int d = cor == Cor.Branca ? -1 : 1;
-
-            pos.definirValores(posicao.linha + d, posicao.coluna);
-            bool casaLivre = tab.posicaoValida(pos) && livre(pos);
-            if (casaLivre)
-                mat[pos.linha, pos.coluna] = true;
-
-            pos.definirValores(posicao.linha + (d * 2), posicao.coluna);
-            if (tab.posicaoValida(pos) && livre(pos) && casaLivre && qteMovimentos == 0)
-                mat[pos.linha, pos.coluna] = true;
-
-            pos.definirValores(posicao.linha + d, posicao.coluna - 1);
-            if (tab.posicaoValida(pos) && existeInimigo(pos))
-                mat[pos.linha, pos.coluna] = true;
-
-            pos.definirValores(posicao.linha + d, posicao.coluna + 1);
-            if (tab.posicaoValida(pos) && existeInimigo(pos))
-                mat[pos.linha, pos.coluna] = true;
-
-            return mat;
-            
-            /*bool[,] mat = new bool[tab.linhas, tab.colunas];
             Posicao pos = new Posicao(0, 0);
             if (cor == Cor.Branca)
             {
@@ -69,6 +48,21 @@ namespace xadrez
                 if (tab.posicaoValida(pos) && existeInimigo(pos))
                 {
                     mat[pos.linha, pos.coluna] = true;
+                }
+
+                //executando en passant #jogadaespecial
+                if (posicao.linha == 3)
+                {
+                    Posicao esquerda = new Posicao(posicao.linha, posicao.coluna - 1);
+                    if (tab.posicaoValida(esquerda) && existeInimigo(esquerda) && tab.peca(esquerda) == partida.vulneravelEnPassant)
+                    {
+                        mat[esquerda.linha - 1, esquerda.coluna] = true;
+                    }
+                    Posicao direita = new Posicao(posicao.linha, posicao.coluna + 1);
+                    if (tab.posicaoValida(direita) && existeInimigo(direita) && tab.peca(direita) == partida.vulneravelEnPassant)
+                    {
+                        mat[direita.linha - 1, direita.coluna] = true;
+                    }
                 }
             }
             else
@@ -93,8 +87,23 @@ namespace xadrez
                 {
                     mat[pos.linha, pos.coluna] = true;
                 }
+
+                //avaliando en passant #jogadaespecial
+                if (posicao.linha == 4)
+                {
+                    Posicao esquerda = new Posicao(posicao.linha, posicao.coluna - 1);
+                    if (tab.posicaoValida(esquerda) && existeInimigo(esquerda) && tab.peca(esquerda) == partida.vulneravelEnPassant)
+                    {
+                        mat[esquerda.linha + 1, esquerda.coluna] = true;
+                    }
+                    Posicao direita = new Posicao(posicao.linha, posicao.coluna + 1);
+                    if (tab.posicaoValida(direita) && existeInimigo(direita) && tab.peca(direita) == partida.vulneravelEnPassant)
+                    {
+                        mat[direita.linha + 1, direita.coluna] = true;
+                    }
+                }
             }
-            return mat;*/
+            return mat;
         }
     }
 }
